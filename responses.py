d76d57b6bc23794b073a03a3d2f95c259a4b3191
roundspecs <2502.zarif@gmail.com>
2022-11-03 08:53:51 +0600

    delete default response

--- responses.py
+++ responses.py
@@ -1,7 +1,5 @@
-def get_response(message: str) -> str:
+def get_response(message: str) -> str | None:
     p_message = message.lower()
 
     if p_message == 'hello':
         return 'hi'
-    
-    return 'Didn\'t understand'
