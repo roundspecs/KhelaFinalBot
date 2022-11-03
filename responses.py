b0249713157194eed35bed530d581d5ae55ffa8c
roundspecs <2502.zarif@gmail.com>
2022-11-03 13:39:28 +0600

    jokes

--- responses.py
+++ responses.py
@@ -3,13 +3,19 @@ from utils import get_handles
 
 
 async def get_response(message, user_message):
+    # Just a joke
+    if str(message.author).startswith("chy_chy") and user_message == "<@693265856305692764>":
+        return "<@693265856305692764>, yasir bhai dake"
+
     p_message = user_message.lower()
 
-    if p_message == "hello":
-        return "hi"
+    if p_message in ["hello", "hi"]:
+        return "ki obostha?"
 
     if p_message == "leaderboard":
-        await message.channel.send("Digging deep into the codeforces database. This may take a while...\nPlease don't type `leaderboard` for the next 5 minutes.\nI'll mention you when I'm done (if nothing goes wrong).")
+        await message.channel.send(
+            "Digging deep into the codeforces database. This may take a while...\nPlease don't type `leaderboard` for the next 5 minutes.\nI'll mention you when I'm done (if nothing goes wrong)."
+        )
         handles = get_handles()
         toppers = leaderboard(handles)
         res = "**Number of problems solved today**```yaml\nNo : CF handle\n--------------\n"
