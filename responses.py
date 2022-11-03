a7e15c2ce0f92ad12139319d9ecaa5f75ff15be5
roundspecs <2502.zarif@gmail.com>
2022-11-03 09:32:33 +0600

    responce to leaderboard

--- responses.py
+++ responses.py
@@ -1,5 +1,20 @@
+from codeforces_api import leaderboard
+
+
 def get_response(message: str) -> str | None:
     p_message = message.lower()
 
-    if p_message == 'hello':
-        return 'hi'
+    if p_message == "hello":
+        return "hi"
+
+    if p_message == "leaderboard":
+        handles = ["sakib.safwan", "ovi_xar", "roundspecs"]
+        toppers = leaderboard(handles)
+        res = "Number of problems solved today```yaml\nNo : CF handle\n"
+        return (
+            res
+            + "\n".join([f"{count:02d} : {handle}" for count, handle in toppers])
+            + "```"
+            + "Congratutaions your majesty, "
+            + toppers[0][1]
+        )
