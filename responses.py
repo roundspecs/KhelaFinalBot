af39f558ddeb81cd9f95b9b7acef04a6db6c6a9b
roundspecs <2502.zarif@gmail.com>
2022-11-04 18:55:53 +0600

    dont change case

--- responses.py
+++ responses.py
@@ -7,22 +7,21 @@ async def get_response(message, user_message):
     if str(message.author).startswith("chy_chy") and user_message == "<@693265856305692764>":
         return "<@693265856305692764>, yasir bhai dake"
 
-    p_message = user_message.lower()
+    if user_message == 'help':
+        return "- type `leaderboard` to see who solved how many problems today\n"\
+        "- type `whats next` to see future plans\n"\
+        "- you can also dm me with the same commands"
 
-    if p_message == 'help':
-        return "- type `whats next` to see future plans\n"\
-        "- type `leaderboard` to see who solved how many problems today"
-
-    if p_message in ["hello", "hi"]:
+    if user_message in ["hello", "hi"]:
         return "ki obostha?"
     
-    if p_message == "whats next":
+    if user_message == "whats next":
         return "**Plans:**\n"\
         "- Make sure everyone solves problems with rating greater than their own rating\n"\
         "- If someone does not solve any problem for 30 consecutive days, remove his/her handle form database\n"\
         "- Automatically show leaderboard everyday at 11:59 PM"
 
-    if p_message == "leaderboard":
+    if user_message == "leaderboard":
         await message.channel.send(
             "Digging deep into the codeforces database. This may take a while...\nPlease don't type `leaderboard` for the next 5 minutes.\nI'll mention you when I'm done (if nothing goes wrong)."
         )
@@ -33,7 +32,7 @@ async def get_response(message, user_message):
             res
             + "\n".join([f"{count:02d} : {handle}" for count, handle in toppers])
             + "```"
-            + "Congratutaions Coder of the day, "
+            + "Congratutaions coder of the day, **"
             + toppers[0][1]
-            + " :crown:"
+            + "** :crown:"
         )
